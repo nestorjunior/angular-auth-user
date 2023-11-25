b06f32cd00e29cb96050afb971ec1fe58597b316
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2023-11-24 21:59:26 -0300

    Update: show user restrict page

--- src/app/restricted-profile/restricted-profile.component.ts
+++ src/app/restricted-profile/restricted-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-restricted-profile',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./restricted-profile.component.scss']
 })
 export class RestrictedProfileComponent {
+  userDetails: any = {};
 
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe((user: any) => {
+      this.userDetails = user;
+      console.log(this.userDetails)
+    })
+  }
 }
